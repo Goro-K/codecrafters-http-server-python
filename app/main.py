cd7ed1f8801eb02b000384e3dbaba7fb8376daa2
Goro-K <romankizilt@gmail.com>
2024-03-26 15:49:17 +0100

    3th stage

--- app/main.py
+++ app/main.py
@@ -3,14 +3,25 @@ import socket
 
 def main():
 
-    server_socket = socket.create_server(("localhost", 4221))
-    conn, addr = server_socket.accept()
-    with conn:
-        print(f"Connected to {addr}")
-        data = conn.recv(1239)
-        conn.send("HTTP/1.1 200 OK\r\n\r\n".encode("utf-8"))
-        
-    server_socket.close()
-    
+    while True: # Pour que le serveur fonctionne sans arrêt même après une requête
+        server_socket = socket.create_server(("localhost", 4221)) # le serveur fonctionne sur le port 4221
+        conn, addr = server_socket.accept()
+        with conn:
+            print(f"Connected to {addr}")
+            data = conn.recv(1239).decode("utf-8") # byte to use split method (conn is str)
+
+            data = data.split("\r\n") 
+            # ['GET /index.html HTTP/1.1', 'Host: localhost:4221', 'User-Agent: curl/7.81.0', 'Accept: */*', '', '']
+            data = data[0].split(" ")
+            # ['GET', '/index.html', 'HTTP/1.1']
+
+            if data[1] == "/" :
+                response = "HTTP/1.1 200 OK\r\n\r\n".encode("utf-8")
+            else:
+                    response = "HTTP/1.1 404 Not Found\r\n\r\n".encode("utf-8")
+            conn.send(response)
+            
+            server_socket.close()
+
 if __name__ == "__main__":
     main()
