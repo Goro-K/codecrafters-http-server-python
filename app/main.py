ce70e70b8ce02360397da0b4fb5fe9f6c605e1d4
Goro-K <romankizilt@gmail.com>
2024-04-03 17:15:29 +0200

    8th stage

--- app/main.py
+++ app/main.py
@@ -1,95 +1,75 @@
-import os
-import socket
-import pathlib
 import argparse
+import socket
+import threading
+from pathlib import Path
 
-from concurrent.futures import ThreadPoolExecutor, wait
-parser = argparse.ArgumentParser(
-    prog="HTTP Server", description="Serves files over HTTP"
-)
-parser.add_argument("-d", "--directory", type=pathlib.Path)
+BUFFER_SIZE = 1024
+ENCODING = "utf-8"
 
-args = parser.parse_args()
-def check_path(data):
-    return data.split(b"\r\n")[0].split(b" ")[1]
+def response_with_content(content, content_type="text/plain", code=200):
+    return f"HTTP/1.1 {code} OK\r\nContent-Type: {content_type}\r\nContent-Length: {len(content)}\r\n\r\n{content}"
 
-def check_method(data):
-    return data.split(b"\r\n")[0].split(b" ")[0]
-
-def get_user_agent(data):
-    for d in data.split(b"\r\n"):
-        if d.startswith(b"User-Agent:"):
-            return d.split(b"User-Agent: ")[1]
-        
-def check_dir(dir, file_name):
-    filepath = os.path.join(dir, file_name)
-
-    return os.path.isfile(filepath)
-
-def get_header_value(data, header):
-    for d in data.split(b"\r\n"):
-        if d.startswith(header):
-            return d.split(b": ")[1]
+def file_response(http_method, path, body, directory):
+    if http_method == "POST":
+        file_name = path.split('/')[-1]
+        file_path = Path(directory) / file_name
+        with open(file_path, "w") as file:
+            file.write(body)
+        return response_with_content('', "text/plain", 201)
+    elif http_method == "GET":
+        file_path = Path(directory) / path.split('/')[-1]
+        if file_path.is_file():
+            return response_with_content(file_path.read_text(), "application/octet-stream")
     return None
 
-def main(server_socket):
-    while True:
-        connection, _ = server_socket.accept()  # accept new connection
+def response(http_method, path, user_agent, body, directory):
+    if path.startswith("/files"):
+        response = file_response(http_method, path, body, directory)
+        if response:
+            return response
+    elif path.startswith("/echo"):
+        random_string = '/'.join(path.split('/')[2:])
+        return response_with_content(random_string)
+    elif path.startswith("/user-agent"):
+        return response_with_content(user_agent)
+    elif path == "/":
+        return "HTTP/1.1 200 OK\r\n\r\n"
+    return "HTTP/1.1 404 Not Found\r\n\r\n"
 
-        data = connection.recv(1024)
-        path = check_path(data)
-        method = check_method(data)
-        print(method)
-        if path == b"/":
-            connection.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-        elif path.startswith(b"/user-agent"):
-            user_agent = get_user_agent(data)
-            connection.sendall(
-                f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n".encode()
-            )
-            connection.sendall(user_agent)
-        elif path.startswith(b"/files"):
-            file_name = path.split(b"/")[2].decode()
-            if not check_dir(dir=args.directory, file_name=file_name):
-                print("not found")
-                connection.sendall("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-                connection.sendall(b"")
-            else:
-                print("sending")
-                d = open(f"{args.directory}/{file_name}", "rb").read()
-                connection.sendall(
-                    f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(d)}\r\n\r\n".encode()
-                )
-                connection.sendall(d)
-        elif method == b"POST" and path.startswith(b'/files'):
-            print("POST")
-            file_name = path.split(b"/")[2].decode()
-            file_path = args.directory / file_name
-            
-            content_length = int(get_header_value(data, b"Content-Length"))
-            body = connection.recv(content_length)
+def parse_request(request):
+    request_lines = request.split("\r\n")
+    http_method, path, _ = request_lines[0].split(" ", 2)
+    user_agent = None
+    for line in request_lines:
+        if line.startswith("User-Agent:"):
+            user_agent = line.split(" ")[1]
+            break
+    body = request_lines[-1] if http_method == "POST" else None
+    return http_method, path, user_agent, body
 
-            # Sauvegarder le contenu dans le fichier
-            with open(file_path, "wb") as file:
-                file.write(body)
-            
-            # Envoyer la réponse 201 Created
-            response = "HTTP/1.1 201 Created\r\n\r\n"
-            connection.sendall(response.encode())
+def handle_client(client_socket, address, directory):
+    data = client_socket.recv(BUFFER_SIZE)
+    request = data.decode(ENCODING)
+    print(f"Request from {address}:\n{request}")
+    http_method, path, user_agent, body = parse_request(request)
+    client_socket.sendall(response(http_method, path, user_agent, body, directory).encode(ENCODING))
+    client_socket.close()
 
-        elif path.startswith(b"/echo"):
-            d = path.split(b"/")[2:]
-            connection.sendall(
-                f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(b'/'.join(d))}\r\n\r\n".encode()
-            )
-            connection.sendall(b"/".join(d))
-        else:
-            connection.sendall("HTTP/1.1 404 NOT FOUND\r\n\r\n".encode())
-        connection.close()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', type=str)
+    args = parser.parse_args()
+    server_socket = socket.create_server(("localhost", 4221))
+    server_socket.listen()
+    try:
+        while True:
+            client_socket, address = server_socket.accept()
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, address, args.directory))
+            client_thread.start()
+    except KeyboardInterrupt:
+        print("Server is shutting down.")
+    finally:
+        server_socket.close()
 
 if __name__ == "__main__":
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # main(server_socket)
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        fs = [executor.submit(main, server_socket) for _ in range(10)]
-        wait(fs)
+    main()
